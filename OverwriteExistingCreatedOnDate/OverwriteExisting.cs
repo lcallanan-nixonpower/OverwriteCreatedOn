dcc9f8bc15c70b61b6cfabd11534c28441fcd3bd
Luke Callanan <lcallanan@nixonpower.com>
2024-02-01 18:30:01 -0600

    Add project files.

---
+++ OverwriteExistingCreatedOnDate/OverwriteExisting.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace OverwriteExistingCreatedOnDate 
+{
+    public class OverwriteExisting : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Obtain the tracing service
+            ITracingService tracingService =
+            (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the execution context from the service provider.  
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // The InputParameters collection contains all the data passed in the message request.  
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.  
+                Entity rental_project = (Entity)context.InputParameters["Target"];
+
+                // Obtain the IOrganizationService instance which you will need for  
+                // web service calls.  
+                IOrganizationServiceFactory serviceFactory =
+                    (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                try
+                {
+                    if(rental_project["new_createdonoverride"] is true)
+                    {
+                        Entity pre_entity = context.PreEntityImages["Image"];
+                        
+                        if(pre_entity["createdon"] != pre_entity["new_sfcreatedon"])
+                        {
+                            rental_project["overriddencreatedon"] = pre_entity["createdon"];
+                            rental_project["createdon"] = pre_entity["new_sfcreatedon"];
+                            rental_project["new_createdonoverride"] = false;
+                        }
+                    }
+                }
+
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in FollowUpPlugin.", ex);
+                }
+
+                catch (Exception ex)
+                {
+                    tracingService.Trace("FollowUpPlugin: {0}", ex.ToString());
+                    throw;
+                }
+            }
+        }
+    }
+}
